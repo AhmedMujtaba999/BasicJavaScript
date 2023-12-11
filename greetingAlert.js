b14867373118af6f61599901f5fe32e68660e4b0
AhmedMujtaba999 <116835811+AhmedMujtaba999@users.noreply.github.com>
2023-12-11 20:01:48 +0530

    Array creation in js
    Basic concepts of arrays are used here such as creation of array , array includes() function

---
+++ greetingAlert.js
@@ -0,0 +1,6 @@
+var guestList=["alia","harry","marco","daniel","suzy"];
+var yourname=prompt("enter your name");
+if(guestList.includes(yourname)){
+ alert("welcome");}
+else{
+alert("sorry,may be next time");}
